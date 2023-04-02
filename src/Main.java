745c9d0c12541ed86f686dec12abe920afe5e7b1
Mateusz Milczarek <milczary.mm@gmail.com>
2023-04-02 13:27:58 +0200

    Finished set up of player ships

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player();
-        player.printMap();
-        player.loadShip(5);
+        try (Scanner scanner = new Scanner(System.in)){
+            Player player = new Player();
+            player.printMap();
+
+            System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
+            player.loadShip(5, scanner);
+            player.printMap();
+
+            System.out.println("Enter the coordinates of the Battleship (4 cells):");
+            player.loadShip(4, scanner);
+            player.printMap();
+
+            System.out.println("Enter the coordinates of the Submarine (3 cells):");
+            player.loadShip(3, scanner);
+            player.printMap();
+
+            System.out.println("Enter the coordinates of the Cruiser (3 cells):");
+            player.loadShip(3, scanner);
+            player.printMap();
+
+            System.out.println("Enter the coordinates of the Destroyer (2 cells):");
+            player.loadShip(2, scanner);
+            player.printMap();
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,9 @@
-import java.security.SignatureException;
 import java.util.Scanner;
 
 public class Player {
     private char[][] playerMap = {
         {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
-        {'~', 'O', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
         {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
         {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
         {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
@@ -19,6 +18,7 @@ public class Player {
     public char[][] getPlayerMap() {
         return playerMap;
     }
+
     public void printMap() {
         System.out.println("  1 2 3 4 5 6 7 8 9 10");
         MapCoordinatesLegend[] firstColumn = MapCoordinatesLegend.values();
@@ -31,43 +31,46 @@ public class Player {
             System.out.println();
         }
     }
-    public boolean loadShip(int size){
-        try (Scanner scanner = new Scanner(System.in)) {
-            do {
-                try {
-                    String startingCoords = scanner.next();
-                    String endingCoords = scanner.next();
-                    char startRow = startingCoords.charAt(0);
-                    char endRow = endingCoords.charAt(0);
-                    int startCol = Integer.parseInt(String.valueOf(startingCoords.charAt(1)));
-                    int endCol = Integer.parseInt(String.valueOf(endingCoords.charAt(1)));
-                    Ship newShip = new Ship(startRow, endRow, startCol, endCol, size);
-                    checkIfCoordsAreNotOccupied(newShip);
-                    totalShips++;
-                    return true;
-                } catch (ShipPlacementException exception) {
-                    System.out.println("Error! " + exception.getMessage() + " Try again:");
-                } catch (NumberFormatException exception) {
-                    System.out.println("Wrong format of coordinates!");
-                } catch (Exception e) {
-                    System.out.println("Wrong format");
-                }
 
-            } while (true);
+    public void loadShip(int size, Scanner scanner) {
+        while (true) {
+            try {
+                String startingCoords = scanner.next();
+                String endingCoords = scanner.next();
+                char startRow = startingCoords.charAt(0);
+                char endRow = endingCoords.charAt(0);
+                int startCol = Integer.parseInt(startingCoords.substring(1));
+                int endCol = Integer.parseInt(endingCoords.substring(1));
+                Ship newShip = new Ship(startRow, endRow, startCol, endCol, size);
+                checkIfCoordsAreNotOccupied(newShip);
+                putShipOnMap(newShip);
+                totalShips++;
+            } catch (ShipPlacementException exception) {
+                System.out.println("Error! " + exception.getMessage() + " Try again:");
+                continue;
+            } catch (NumberFormatException exception) {
+                System.out.println("Wrong format of coordinates!");
+            } catch (Exception e) {
+                System.out.println("Wrong format");
+            }
+            return;
         }
     }
+
     private void checkIfCoordsAreNotOccupied(Ship ship) throws ShipPlacementException {
-        if (ship.getPositioning().equals("vertically")) {
-            checkIfCoordsAreNotOccupiedVertically(ship);
-        }
-        else {
-            checkIfCoordsAreNotOccupiedHorizontally(ship);
-        }
-    }
-    private void checkIfCoordsAreNotOccupiedVertically(Ship ship) throws ShipPlacementException {
         int y = ship.getyStartingShipCoordinate();
         int x = ship.getxStartingShipCoordinate();
-        //todo last coded
+        if (ship.getPositioning().equals("vertically")) {
+            checkIfCoordsAreNotOccupiedVertically(ship, y, x);
+        }
+        else {
+            checkIfCoordsAreNotOccupiedHorizontally(ship, y, x);
+        }
+    }
+
+    private void checkIfCoordsAreNotOccupiedVertically(Ship ship, int y, int x)
+            throws ShipPlacementException {
+
         try {
             if (playerMap[y-1][x] == 'O') {
                 throw new ShipPlacementException("You placed it too close to another one.");
@@ -92,24 +95,67 @@ public class Player {
             catch (IndexOutOfBoundsException e) {}
         }
         try {
-            if (playerMap[y+1][x] == 'O') {
+            if (playerMap[y][x] == 'O') {
                 throw new ShipPlacementException("You placed it too close to another one.");
             }
         }
         catch (IndexOutOfBoundsException e) {}
     }
-    private void checkIfCoordsAreNotOccupiedHorizontally(Ship ship) throws ShipPlacementException {
 
+    private void checkIfCoordsAreNotOccupiedHorizontally(Ship ship, int y, int x)
+            throws ShipPlacementException {
+
+        try {
+            if (playerMap[y][x-1] == 'O') {
+                throw new ShipPlacementException("You placed it too close to another one.");
+            }
+        }
+        catch (IndexOutOfBoundsException e) {}
+        for (int i=0; i<ship.getSize(); i++, x++) {
+            if (playerMap[y][x] == 'O') {
+                throw new ShipPlacementException("Coordinate already occupied!");
+            }
+            try {
+                if (playerMap[y-1][x] == 'O'){
+                    throw new ShipPlacementException("You placed it too close to another one.");
+                }
+            }
+            catch (IndexOutOfBoundsException e) {}
+            try {
+                if (playerMap[y+1][x] == 'O'){
+                    throw new ShipPlacementException("You placed it too close to another one.");
+                }
+            }
+            catch (IndexOutOfBoundsException e) {}
+        }
+        try {
+            if (playerMap[y][x] == 'O') {
+                throw new ShipPlacementException("You placed it too close to another one.");
+            }
+        }
+        catch (IndexOutOfBoundsException e) {}
     }
 
     private void putShipOnMap(Ship ship) {
+        int y = ship.getyStartingShipCoordinate();
+        int x = ship.getxStartingShipCoordinate();
         if (ship.getPositioning().equals("vertically")) {
-            putShipOnMapVertically();
+            putShipOnMapVertically(ship, y, x);
         }
         else {
-            putShipOnMapHorizontally();
+            putShipOnMapHorizontally(ship, y, x);
         }
     }
 
+    private void putShipOnMapVertically(Ship ship, int y, int x) {
+        for (int i=0; i<ship.getSize(); i++, y++) {
+            playerMap[y][x] = 'O';
+        }
+    }
 
+    private void putShipOnMapHorizontally(Ship ship, int y, int x) {
+        for (int i=0; i<ship.getSize(); i++, x++) {
+            playerMap[y][x] = 'O';
+        }
+    }
 }
